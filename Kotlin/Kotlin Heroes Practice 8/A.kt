4cc66d9c23e5ed520ab2fff2364c48d7fdc9a0cc
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2021-10-08 16:12:22 +0600

    Add files via upload

---
+++ Kotlin/Kotlin Heroes Practice 8/A.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+fun main() {
+    var inp=Scanner(System.`in`)
+    var t=inp.nextInt()
+    while(t-->0)
+    {
+        var a=inp.nextInt()
+        var b=inp.nextInt()
+        println(a+b)
+    }
+}
