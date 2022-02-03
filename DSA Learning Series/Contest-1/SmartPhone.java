946dff413bc48fd5a8a0ed9c8e9e8bea2e979011
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2022-02-03 22:14:46 +0600

    Add files via upload

---
+++ DSA Learning Series/Contest-1/SmartPhone.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class SmartPhone{
+    
+    public static void main(String[] args)
+    {
+        
+        Scanner inp=new Scanner(System.in);
+        int n=inp.nextInt();
+        
+        long a[]=new long[n];
+        long maxi=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            a[i]=inp.nextInt();
+        }
+        
+        Arrays.sort(a);
+        
+        int len=n;
+        
+        for(int i=0;i<n;i++)
+        {
+           maxi=Math.max(maxi,a[i]*len);
+           len--;
+        }
+        
+        System.out.println(maxi);
+        
+    }
+    
+    
+}
