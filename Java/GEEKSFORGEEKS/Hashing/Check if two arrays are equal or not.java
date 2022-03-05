f271fcfa4d5a6ade591121b2c1dd3565db4d9e59
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2022-03-05 10:46:21 +0600

    Add files via upload

---
+++ Java/GEEKSFORGEEKS/Hashing/Check if two arrays are equal or not.java
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long arr[]=new long[n];
+		  long brr[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      arr[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      brr[i]=sc.nextLong();
+		  }
+		  Solution ob = new Solution();
+		  System.out.println(ob.check(arr,brr,n)==true?"1":"0");
+		  
+		  
+		}
+	}
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        //Your code here
+        
+        HashMap<Long, Long> tm1 = new HashMap<Long, Long>();
+        HashMap<Long, Long> tm2 = new HashMap<Long, Long>();
+
+        for (int i = 0; i < N; i++) {
+            if (tm1.containsKey(A[i]))
+                tm1.put(A[i], tm1.get(A[i]) + 1L);
+
+            else {
+                tm1.put(A[i], 1L);
+            }
+
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (tm2.containsKey(B[i]))
+                tm2.put(B[i], tm2.get(B[i]) + 1L);
+
+            else {
+                tm2.put(B[i], 1L);
+            }
+
+        }
+
+        return tm1.equals(tm2);
+}
+}
