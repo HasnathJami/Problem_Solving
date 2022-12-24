3af90b2309e09452b4491577c509ac291d0afa73
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2022-12-25 04:05:53 +0600

    Add files via upload

---
+++ Java/Codeforces/ExtremelyRound.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class ExtremelyRound {
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int t = inp.nextInt();
+        while (t-- > 0) {
+            int n = inp.nextInt();
+            int digitCount = 0;
+            int firstDigit = 0;
+            while (n > 0) {
+                firstDigit = n;
+                n = n / 10;
+                digitCount++;
+            }
+            System.out.println((digitCount - 1) * 9 + firstDigit);
+        }
+    }
+}
